3adbb401c1ccf5b948bb19b765c8ee8940a35a7b
wntjdjus <wntjdjus@naver.com>
2021-02-14 02:32:32 +0900

    [Mod] 테스트 코드 수정

--- src/test/java/com/jutudy/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jutudy/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.jutudy.book.springboot.web;
 
+import com.jutudy.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,31 +16,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
-    private MockMvc mvc;    // 웹 API 테스트 시 사용
+    private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
-    public void returnHello() throws Exception{
+    public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
-        mvc.perform(get("/hello"))            // GET 요청
-                .andExpect(status().isOk())             // HTTP header 검증
-                .andExpect(content().string(hello));    // 응답 본문 검증
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
-    public void returnHelloJson() throws Exception {
+    public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
         mvc.perform(
                 get("/hello/dto")
-                .param("name", name)
-                .param("amount", String.valueOf(amount))
-        )
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
